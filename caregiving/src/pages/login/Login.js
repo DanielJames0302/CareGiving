ad70e32c8c5c66bd4020462cd09c90691c794a6d
mman <minhmanuni@gmail.com>
2024-02-04 13:51:48 +0800

    create login form

--- caregiving/src/pages/login/Login.js
+++ caregiving/src/pages/login/Login.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import LoginForm from '../../components/login/login-form';
-
+import './Login.css'
 const Login = () => {
   return (
     <div className='login-page'>
-      <div className="login-page-content">
+      <div className="login-page-wrapper">
           <LoginForm />
       </div>
     </div>
